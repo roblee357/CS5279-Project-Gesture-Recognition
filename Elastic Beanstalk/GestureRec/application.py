32a68d49657f57b587579375bf751df00801b25a
roblee357 <roblee357@hotmail.com>
2021-06-14 17:43:57 -0400

    trying so hard

--- Elastic Beanstalk/GestureRec/application.py
+++ Elastic Beanstalk/GestureRec/application.py
@@ -1,7 +1,8 @@
-from flask import Flask, request
+from flask import Flask, request, send_file, abort, render_template
 from flask_restful import Resource, Api
-import json
-import get_video
+import json, os
+
+# import get_video
 
 # print a nice greeting.
 def say_hello(username = "World"):
@@ -29,15 +30,51 @@ def newclip():
    title  = request.args.get('title', None)
    cstart  = request.args.get('cstart', None)
    cend  = request.args.get('cend', None)
-#    print('cID',cID,'title',title,'cstart',cstart,'cend',cend)
+   print('cID',cID,'title',title,'cstart',cstart,'cend',cend)
    mstring =  {'cID':  cID , 'title' : title , 'cstart' : cstart, 'cend': cend}
-   video = get_video.vid(cID,title,cstart,cend)
-   video.detect()
+   with open('output.txt','w') as fout:
+       fout.write(json.dumps(mstring))
+#    video = get_video.vid(cID,title,cstart,cend)
+#    video.detect()
    return json.dumps({'success':True, 'input':mstring}), 200, {'ContentType':'application/json'}
+
+
+@app.route('/files')
+def dir_listing():
+    # BASE_DIR = '/'
+
+
+
+    # # Return 404 if path doesn't exist
+    # if not os.path.exists(abs_path):
+    #     return abort(404)
+
+    # Check if path is a file and serve
+
+
+    # Show directory contents
+    files = os.listdir(os.getcwd())
+    return render_template('files.html', files=files)
+
+@app.route('/', defaults={'req_path': ''})
+@app.route('/<path:req_path>')
+def dir_listing2(req_path):
+    BASE_DIR = os.getcwd()
+    # Joining the base and the requested path
+    abs_path = os.path.join(BASE_DIR, req_path)
+
+    # # Joining the base and the requested path
+    # abs_path = os.path.join(BASE_DIR, req_path)
+    if os.path.isfile(abs_path):
+        return send_file(abs_path)
+    else:
+        return abs_path
+
 
 # run the app.
 if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
     application.debug = True
+    
     application.run()
